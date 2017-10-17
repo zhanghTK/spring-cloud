c3abcf63967d044b7063af071fbcec5e71e37a52
zhanghTk <zhangh.tk@gmail.com>
2017-10-17 14:55:48 +0800

    add Turbine 使用

---
+++ 10_turbine/src/main/java/tk/zhangh/springcloud/Application.java
@@ -0,0 +1,16 @@
+package tk.zhangh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+@EnableHystrixDashboard
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
